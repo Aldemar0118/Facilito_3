d66f384d5c2be2a79acd8198f9aef3a59b24307b
arthurocrates <arthurocrates@hotmail.com>
2021-10-06 01:38:42 +0000

    redireccion home

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import RegistroProducto from '../views/RegistroProducto.vue'
+import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/registroproducto",
     name: "RegistroProducto",
     component: RegistroProducto,
   },
