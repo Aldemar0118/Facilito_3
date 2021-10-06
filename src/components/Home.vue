eecdbd1a204ec30498da629bbf632e16e1c611ad
arthurocrates <arthurocrates@hotmail.com>
2021-10-06 01:32:23 +0000

    home

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+
+    <table>
+    <thead>
+<tr>
+<td colspan="2">
+ <h1>{{ msg }}</h1>
+</td>
+</tr>
+</thead>
+
+
+
+
+
+
+
+</table>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+        <Home msg="Hola mindo"/>
+       
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Home from '@/components/Home.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Home
+  }
+}
+</script>
